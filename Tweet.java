ab4667bfc80a440faaf208632739868bac66d5f2
Fabian <fberress@example.com>
2017-02-04 22:13:58 +0100

    Added a function to read a string to a tweet. Can be used for reading the pre-processed data for further analysis.

--- Tweet.java
+++ Tweet.java
@@ -2,6 +2,8 @@ package bigdata;
 import java.lang.StringBuilder;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.lang.Exception;
 
 public class Tweet implements Serializable{
 	
@@ -13,7 +15,7 @@ public class Tweet implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private String location;
+	//private String location;
 	private String date;
 	private String text;
 	private ArrayList<String> hashtags;
@@ -23,7 +25,7 @@ public class Tweet implements Serializable{
 	private String device;
 	
 	Tweet(){
-		location = "";
+		//location = "";
 		date = "";
 		text = "";
 		hashtags = new ArrayList<String>();
@@ -107,4 +109,81 @@ public class Tweet implements Serializable{
 	public void setDate(String date) {
 		this.date = date;
 	}
+	
+	public String getDevice() {
+		return device;
+	}
+
+	public void setDevice(String device) {
+		this.device = device;
+	}
+	
+	public Tweet readStringToTweet(String s) throws StringToTweetConversionException{
+		Tweet tmp = new Tweet();
+		StringTokenizer tok = new StringTokenizer(s, "\t");
+		
+		if(tok.hasMoreTokens()){
+			tmp.setDate(tok.nextToken());
+		}
+		else{
+			throw new StringToTweetConversionException("No date found.");
+		}
+		
+		if(tok.hasMoreTokens()){
+			tmp.setText(tok.nextToken());
+		}
+		else{
+			throw new StringToTweetConversionException("No text found.");
+		}
+		
+		if(tok.hasMoreTokens()){
+			ArrayList<String> hashtagArray = new ArrayList<String>();
+			StringTokenizer tok2 = new StringTokenizer(tok.nextToken());
+			while(tok2.hasMoreTokens()){
+				String tag = tok2.nextToken();
+				if(!tag.equals("[") && !tag.equals("]")){
+					hashtagArray.add(tag);
+				}
+			}
+			tmp.setHashtags(hashtagArray);
+		}
+		else{
+			throw new StringToTweetConversionException("No hashtag-array found.");
+		}
+		
+		if(tok.hasMoreTokens()){
+			tmp.setNumberOfRetweets(Integer.parseInt(tok.nextToken()));
+		}
+		else{
+			throw new StringToTweetConversionException("No number of retweets found.");
+		}
+		
+		if(tok.hasMoreTokens()){
+			tmp.setNumberOfLikes(Integer.parseInt(tok.nextToken()));
+		}
+		else{
+			throw new StringToTweetConversionException("No number of likes found.");
+		}
+		
+		if(tok.hasMoreTokens()){
+			tmp.setNumberOfFollowers(Integer.parseInt(tok.nextToken()));
+		}
+		else{
+			throw new StringToTweetConversionException("No number of followers found.");
+		}
+		if(tok.hasMoreTokens()){
+			tmp.setDevice(tok.nextToken());
+		}
+		else{
+			throw new StringToTweetConversionException("No device found.");
+		}
+		
+		return tmp;
+	}
+}
+
+class StringToTweetConversionException extends Exception {
+   public StringToTweetConversionException(String msg){
+      super(msg);
+   }
 }
